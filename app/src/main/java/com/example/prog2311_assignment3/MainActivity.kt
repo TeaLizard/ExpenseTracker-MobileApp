da6b976b4d1998a1751594a459e5b6a902d141a4
TeaLizard <owentwilliams7@gmail.com>
2025-11-17 15:58:59 -0500

    first commit

---
+++ app/src/main/java/com/example/prog2311_assignment3/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.prog2311_assignment3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.prog2311_assignment3.feature.ExpenseTracker
+import com.example.prog2311_assignment3.ui.theme.PROG2311_Assignment3Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PROG2311_Assignment3Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    ExpenseTracker(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/prog2311_assignment3/feature/ExpenseTracker.kt
@@ -0,0 +1,9 @@
+package com.example.prog2311_assignment3.feature
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ExpenseTracker(modifier: Modifier = Modifier) {
+
+}
