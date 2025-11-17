8fedd2b21837ea398900eee8cccaf6a60f1c4d22
TeaLizard <owentwilliams7@gmail.com>
2025-11-17 16:26:54 -0500

    Added expense entity and database helper

---
+++ app/src/main/java/com/example/prog2311_assignment3/data/Expense.kt
@@ -0,0 +1,8 @@
+package com.example.prog2311_assignment3.data
+
+data class Expense(
+    val id: Int,
+    val title: String,
+    val amount: Int,
+    val category: String
+)
---
+++ app/src/main/java/com/example/prog2311_assignment3/data/ExpenseDatabaseHelper.kt
@@ -0,0 +1,94 @@
+package com.example.prog2311_assignment3.data
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class ExpenseDatabaseHelper(context: Context)
+    : SQLiteOpenHelper(context, "expenses", null, 1) {
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL("""
+            CREATE TABLE expenses (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT,
+                amount INTEGER,
+                category TEXT
+            )
+        """.trimIndent())
+    }
+
+    override fun onUpgrade(
+        db: SQLiteDatabase?,
+        p1: Int,
+        p2: Int
+    ) {
+        db?.execSQL("DROP TABLE IF EXISTS expenses")
+        onCreate(db)
+    }
+
+    fun addExpense(expense: Expense) {
+        val db = writableDatabase
+        val values = ContentValues().apply {
+            put("title", expense.title)
+            put("amount", expense.amount)
+            put("category", expense.category)
+        }
+        db.insert("expenses", null, values)
+        db.close()
+    }
+
+    fun getExpenses(): List<Expense> {
+        val expenses = mutableListOf<Expense>()
+        val db = readableDatabase
+        val cursor = db.rawQuery("SELECT * FROM expenses", null)
+        while (cursor.moveToNext()) {
+            expenses.add(
+                Expense(
+                    cursor.getInt(cursor.getColumnIndexOrThrow("id")),
+                    cursor.getString(cursor.getColumnIndexOrThrow("title")),
+                    cursor.getInt(cursor.getColumnIndexOrThrow("amount")),
+                    cursor.getString(cursor.getColumnIndexOrThrow("category"))
+                )
+            )
+        }
+        cursor.close()
+        db.close()
+        return expenses
+    }
+
+    fun getExpenseById(id: Int): Expense? {
+        val db = readableDatabase
+        val cursor = db.rawQuery("SELECT * FROM expenses WHERE id = ?", arrayOf(id.toString()))
+        // if there is a record at that id, return it, else return null
+        val note = if (cursor.moveToFirst()) {
+            Expense(
+                cursor.getInt(cursor.getColumnIndexOrThrow("id")),
+                cursor.getString(cursor.getColumnIndexOrThrow("title")),
+                cursor.getInt(cursor.getColumnIndexOrThrow("amount")),
+                cursor.getString(cursor.getColumnIndexOrThrow("category"))
+            )
+        } else null
+        cursor.close()
+        db.close()
+        return note
+    }
+
+    fun updateExpense(expense: Expense) {
+        val db = writableDatabase
+        val values = ContentValues().apply {
+            put("title", expense.title)
+            put("amount", expense.amount)
+            put("category", expense.category)
+        }
+        db.update("expenses", values, "id = ?", arrayOf(expense.id.toString()))
+        db.close()
+    }
+
+    fun deleteExpenseById(id: Int) {
+        val db = writableDatabase
+        db.delete("expenses", "id = ?", arrayOf(id.toString()))
+        db.close()
+    }
+}
